4c0a1a499e8469bbc94dcde8ec97cf6b4b4ffffa
lukejian003 <lukejian@live.com>
2019-06-23 21:25:22 +0800

    完成布局&gt.js的引入

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,20 +1,85 @@
 <template>
-    <div>
-        <p>AppLogin</p>
+  <div class="login-warp">
+    <div class="login-from-warp">
+      <div class="login-head">
+        <img src="./logo_index.png" alt="黑马头条">
+      </div>
+      <div class="login-from">
+        <el-form ref="form" :model="form">
+          <el-form-item>
+            <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-col :span="14">
+              <el-input v-model="form.code" placeholder="验证码"></el-input>
+            </el-col>
+            <el-col :span="6" :offset="1">
+              <el-button @click="handleSendCode">发送验证码</el-button>
+            </el-col>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit" id="dl">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import '@/vendor/gt.js'
 export default {
   name: 'AppLogin',
   data () {
     return {
-
+      form: {
+        mobile: '17777777777',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    handleSendCode () {
+      const { mobile } = this.form
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        console.log(res.data)
+      })
     }
   }
 }
 </script>
 
 <style lang="less" scopend>
-
+.login-warp {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: url(./login_bg.jpg) repeat 0, 0;
+  background-size: 100%;
+  .login-from-warp {
+    padding: 50px;
+    background-color: #fff;
+    .login-head {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 20px;
+      img {
+        width: 60%;
+        height: 60%;
+      }
+    }
+  }
+  #dl {
+    width: 100%;
+  }
+}
 </style>
